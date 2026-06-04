cfc8025997bbb1db05e647b102a2d1c5bfddca56
Leonardo de Souza <156200672+lstorrent@users.noreply.github.com>
2026-06-04 12:24:47 -0300

    fix: ajuste de respawn de zumbis no SandboxVars

--- configs/servertest_SandboxVars.lua
+++ configs/servertest_SandboxVars.lua
@@ -596,7 +596,7 @@ SandboxVars = {
         -- The day when the population reaches it's peak. Minimum=1 Maximum=365 Default=28
         PopulationPeakDay = 28,
         -- The number of hours that must pass before zombies may respawn in a cell. If zero, spawning is disabled. Minimum=0.00 Maximum=8760.00 Default=72.00
-        RespawnHours = 72.0,
+        RespawnHours = 0.0,
         -- The number of hours that a chunk must be unseen before zombies may respawn in it. Minimum=0.00 Maximum=8760.00 Default=16.00
         RespawnUnseenHours = 16.0,
         -- The fraction of a cell's desired population that may respawn every RespawnHours. Minimum=0.00 Maximum=1.00 Default=0.10
